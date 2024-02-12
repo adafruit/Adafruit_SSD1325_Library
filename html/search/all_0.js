827fcaadef84979bcf16a062cd888cbc4496db8d
Doxygen CI <ci-arduino@invalid>
2024-02-12 15:48:43 +0000

    Deploy docs to GitHub Pages from commit c5e41c4cf4
    Commit: c5e41c4cf48043385b3440585f997ba3f3a98719
    GitHub Actions run: 7874239524

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -4,6 +4,5 @@ var searchData=
   ['adafruit_5fssd1325_2ecpp',['Adafruit_SSD1325.cpp',['../_adafruit___s_s_d1325_8cpp.html',1,'']]],
   ['adafruit_5fssd1325_2eh',['Adafruit_SSD1325.h',['../_adafruit___s_s_d1325_8h.html',1,'']]],
   ['adafruit_5fssd1325_5fspi',['ADAFRUIT_SSD1325_SPI',['../_adafruit___s_s_d1325_8cpp.html#abf433bd4daf83998e7da4410af7ea9c1',1,'Adafruit_SSD1325.cpp']]],
-  ['adagfx_5fswap',['adagfx_swap',['../_adafruit___s_s_d1325_8h.html#ac096d111acea06d69cd69f30c0e30541',1,'Adafruit_SSD1325.h']]],
   ['adafruit_20ssd1325_20library',['Adafruit SSD1325 Library',['../index.html',1,'']]]
 ];
--- html/search/defines_0.js
+++ html/search/defines_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fssd1325_5fspi',['ADAFRUIT_SSD1325_SPI',['../_adafruit___s_s_d1325_8cpp.html#abf433bd4daf83998e7da4410af7ea9c1',1,'Adafruit_SSD1325.cpp']]],
-  ['adagfx_5fswap',['adagfx_swap',['../_adafruit___s_s_d1325_8h.html#ac096d111acea06d69cd69f30c0e30541',1,'Adafruit_SSD1325.h']]]
+  ['adafruit_5fssd1325_5fspi',['ADAFRUIT_SSD1325_SPI',['../_adafruit___s_s_d1325_8cpp.html#abf433bd4daf83998e7da4410af7ea9c1',1,'Adafruit_SSD1325.cpp']]]
 ];
